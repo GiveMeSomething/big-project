f96c337a712dc662f78c1dcc2b07577ef670babc
Hoang Minh <minhbeo135@gmail.com>
2018-06-20 15:16:01 +0700

    fix bug in control.js

--- src/controller.js
+++ src/controller.js
@@ -1,5 +1,6 @@
 var myVideo = document.getElementById("videoPlayer");
 var myPhone = document.getElementById("myPhone");
+
 function playAnimation() {
     myPhone.style.top = '1800px';
     myPhone.style.transition = 'transform 2s';
