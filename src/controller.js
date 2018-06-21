4a601292e3100387dc09237f368935d9541f7fd5
Hoang Minh <minhbeo135@gmail.com>
2018-06-21 08:03:30 +0700

    add more transitions in "Vo Cuc" part

--- src/controller.js
+++ src/controller.js
@@ -1,9 +1,14 @@
-var myVideo = document.getElementById("videoPlayer");
-var myPhone = document.getElementById("myPhone");
+const myVideo = document.getElementById("videoPlayer");
+const myPhone = document.getElementById("myPhone");
 
 function playAnimation() {
     myPhone.style.top = '1800px';
+
     myPhone.style.transition = 'transform 2s';
-    myPhone.style.transform = 'translateY(-850px)';
+    myVideo.style.transition = 'transform 2s';
+
     myVideo.play();
+    myVideo.style.transform = 'scale(0.75)';
+    myPhone.style.transform = 'translateY(-850px) scale(0.75)';
+
 }
