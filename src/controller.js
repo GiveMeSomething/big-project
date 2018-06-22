b0545c4a955f6fb729bb38df584a25a6b7ab9fe7
Hoang Minh <minhbeo135@gmail.com>
2018-06-22 15:36:00 +0700

    add transition for "S Pen" part

--- src/controller.js
+++ src/controller.js
@@ -1,14 +1,43 @@
-const myVideo = document.getElementById("videoPlayer");
-const myPhone = document.getElementById("myPhone");
+const videoVoCUc = document.getElementById("videoPlayer");
+const phoneVoCuc = document.getElementById("myPhone");
+
+const imageSPen = document.getElementById("imageSPen");
+const butSPen = document.getElementById("butSPen");
+
+const sPenPart_1 = document.getElementById("sPenPart_1");
+const sPenPart_2 = document.getElementById("sPenPart_2");
 
 function playAnimation() {
-    myPhone.style.top = '1800px';
+    phoneVoCuc.style.top = '1800px';
 
-    myPhone.style.transition = 'transform 2s';
-    myVideo.style.transition = 'transform 2s';
+    phoneVoCuc.style.transition = 'transform 1.5s';
+    videoVoCUc.style.transition = 'transform 2s';
 
-    myVideo.play();
-    myVideo.style.transform = 'scale(0.75)';
-    myPhone.style.transform = 'translateY(-850px) scale(0.75)';
+    videoVoCUc.play();
+    videoVoCUc.style.transform = 'scale(0.5)';
+    phoneVoCuc.style.transform = 'translateY(-850px) scale(0.75)';
 
+}
+
+function changeText() {
+    if (window.pageYOffset > 3400 && window.pageYOffset < 4000) {
+        imageSPen.style.transition = 'transform 1s';
+        imageSPen.style.transform = 'translateY(-830px)';
+
+        sPenPart_1.style.visibility = 'hidden';
+
+        sPenPart_2.style.visibility = 'visible';
+        sPenPart_2.style.transition = 'transform 1s';
+        sPenPart_2.style.transform = 'translateY(500px)';
+    }
+    else {
+        sPenPart_1.style.visibility = 'visible';
+
+        sPenPart_2.style.visibility = 'hidden';
+        sPenPart_2.style.transform = 'translateY(-500px);'
+    }
+}
+
+function alertPosition() {
+    alert(window.pageYOffset);
 }
