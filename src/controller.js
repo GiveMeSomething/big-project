aaf8f85f524babef3e40f4b0d98197a58a427bce
Hoang Minh <minhbeo135@gmail.com>
2018-06-24 23:02:23 +0700

    add transition in 'Camera' part

--- src/controller.js
+++ src/controller.js
@@ -2,10 +2,14 @@ const videoVoCUc = document.getElementById("videoPlayer");
 const phoneVoCuc = document.getElementById("myPhone");
 
 const imageSPen = document.getElementById("imageSPen");
-const butSPen = document.getElementById("butSPen");
 
 const sPenPart_1 = document.getElementById("sPenPart_1");
 const sPenPart_2 = document.getElementById("sPenPart_2");
+
+const imageCamera = document.getElementById("imageCamera");
+
+const cameraPart_1 = document.getElementById("text1_Camera");
+const cameraPart_2 = document.getElementById("text2_Camera");
 
 function playAnimation() {
     phoneVoCuc.style.top = '1800px';
@@ -19,8 +23,8 @@ function playAnimation() {
 
 }
 
-function changeText() {
-    if (window.pageYOffset > 3400 && window.pageYOffset < 4000) {
+function changeTextSPen() {
+    if (window.pageYOffset > 3550 && window.pageYOffset < 4500) {
         imageSPen.style.transition = 'transform 1s';
         imageSPen.style.transform = 'translateY(-830px)';
 
@@ -31,11 +35,35 @@ function changeText() {
         sPenPart_2.style.transform = 'translateY(500px)';
     }
     else {
+        imageSPen.style.transform = 'translateY(830px)';
+
         sPenPart_1.style.visibility = 'visible';
 
         sPenPart_2.style.visibility = 'hidden';
         sPenPart_2.style.transform = 'translateY(-500px);'
     }
+}
+
+function changeTextCamera() {
+    if (window.pageYOffset > 5400 && window.pageYOffset < 5800) {
+        cameraPart_1.style.visibility = 'hidden';
+
+        cameraPart_2.style.transition = 'transform 1s';
+        cameraPart_2.style.visibility = 'visible';
+        cameraPart_2.style.transform = 'translateY(300px)';
+
+        imageCamera.style.transition = 'transform 1s';
+        imageCamera.style.transform = 'translateY(-700px)';
+    }
+    else {
+        cameraPart_1.style.visibility = 'visible';
+
+        cameraPart_2.style.visibility = 'hidden';
+        cameraPart_2.style.transform = 'translateY(-300px)';
+
+        imageCamera.style.transform = 'translateY(700px)';
+    }
+
 }
 
 function alertPosition() {
